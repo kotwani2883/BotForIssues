11ec32e98736396ec1fda73a813105d3eab615cb
kotwani2883 <palakkotwani2883@gmail.com>
2023-04-05 11:37:46 +0530

    Use case covered

--- index.js
+++ index.js
@@ -2,149 +2,133 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Octokit } = require('@octokit/rest');
 const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
 
 const app = express();
+const mock = new MockAdapter(axios);
 
 // Github API authentication
 const octokit = new Octokit({
-  auth: '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'
+  auth: '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'
 });
 
 app.use(bodyParser.json());
-app.get('/hello',(req,res)=>{
-  res.send("hello World");
-})
+
 // Webhook endpoint to receive events from Github
 app.post('/webhook', async (req, res) => {
   const payload = req.body;
-  console.log('Received payload:', payload);
   // Handle the payload as needed
   const { action, issue } = req.body;
-   console.log(action);
   if (action === 'closed' && payload.pull_request.merged) {
     // If an issue is opened, create a comment on it
     const comment = {
       body: 'Thanks for opening this issue!',
     };
-console.log(payload.repository.owner.login)
-console.log(payload.repository.name)
-console.log(payload.number)
-console.log( comment.body)
 // 1.comment on a specific Issue
-    await octokit.issues.createComment({
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      issue_number: payload.number,
-      body: comment.body,
-    });
+const url = 'https://smee.io/aWMY6G3acItkWaAq';
+const data = {
+  title: payload.pull_request.title,
+  body: payload.pull_request.body
+};
+const headers = {
+  'Content-Type': 'application/json',
+};
 
-// 2. Comment on a specific commit
-// octokit.rest.repos.createCommitComment({
-//   owner,
-//   repo,
-//   commit_sha: payload.pull_request. merge_commit_sha,
-//   body: comment,
-// })
-// .then(response => {
-//   console.log('Comment created successfully:', response.data.html_url);
-// })
-// .catch(error => {
-//   console.error('Error creating comment:', error);
-// });
+
+// Mock the HTTP POST request
+mock.onPost('https://smee.io/aWMY6G3acItkWaAq').reply(200, {
+  message: 'Mock response from Smee.io',
+});
+try {
+  const response = await axios.post(url, data, { headers });
+  console.log(response.data);
+  const commentBody = `The third-party API responded with: ${JSON.stringify(response.data)}`; 
+
+  //1.comment on specific PR
+  await octokit.issues.createComment({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    issue_number: payload.pull_request.number,
+    body: commentBody,
+  });
+
+
+  // 2. Comment on a specific commit
+await octokit.rest.repos.createCommitComment({
+  owner:payload.repository.owner.login,
+  repo: payload.repository.name,
+  commit_sha: payload.pull_request. merge_commit_sha,
+  body: commentBody,
+})
+.then(response => {
+  console.log('Comment created successfully:', JSON.stringify(response.data));
+})
+.catch(error => {
+  console.error('Error creating comment:', error);
+});
+
 
 //3. Write/Add a new file to the repo 
-// const owner = 'OWNER_USERNAME';
-// const repo = 'REPO_NAME';
-// const path = 'PATH_TO_NEW_FILE'; // Include file name and extension
-// const content = 'FILE_CONTENTS_HERE';
-// const messagetoadd = 'COMMIT_MESSAGE_HERE';
-// const branch = 'BRANCH_NAME'; // Optional - defaults to repository's default branch
+await octokit.rest.repos.createOrUpdateFileContents({
+  owner: payload.repository.owner.login,
+  repo: payload.repository.name,
+  path: 'path/to/file.txt',
+  message: 'Add comment',
+  content: Buffer.from(commentBody).toString('base64'),
+  branch: payload.repository.default_branch,
+})
+.then(response => {
+  console.log('File created successfully:', response.data.content.html_url);
+})
+.catch(error => {
+  console.error('Error creating file:', error);
+});
 
-// octokit.rest.repos.createOrUpdateFileContents({
-//   owner,
-//   repo,
-//   path,
-//   messagetoadd,
-//   content: Buffer.from(content).toString('base64'),
-//   branch,
-// })
-// .then(response => {
-//   console.log('File created successfully:', response.data.content.html_url);
-// })
-// .catch(error => {
-//   console.error('Error creating file:', error);
-// });
 
 // // 4.Alter an existing file in the repo 
-// const ownerInfo = "OWNER_USERNAME";
-// const repoName = "REPO_NAME";
-// const pathofFile = "FILE_PATH";
+const currentContent = await octokit.repos.getContent({
+  owner:payload.repository.owner.login,
+  repo:payload.repository.name,
+  path: 'path/to/file.txt',
+});
 
-// const result = await octokit.repos.getContents({
-//   ownerInfo,
-//   repoName,
-//   pathofFile
-// });
+// update the content of the file
+const newContent = "This is the new content of the file.";
+const updateResult = await octokit.repos.createOrUpdateFileContents({
+  owner:payload.repository.owner.login,
+  repo:payload.repository.name,
+  path: 'path/to/file.txt',
+  message: "Update file",
+  content: Buffer.from(newContent).toString("base64"),
+  sha: currentContent.data.sha,
+});
 
-// const file = Buffer.from(result.data.content, "base64").toString();
-// const sha = result.data.sha;
-// const newFile = file.replace("OLD_STRING", "NEW_STRING");
-// const messageOfCommit = "Commit message";
-// const contentOfFile = Buffer.from(newFile).toString("base64");
-
-// await octokit.repos.createOrUpdateFileContents({
-//   ownerInfo,
-//   repoName,
-//   pathofFile,
-//   messageOfCommit,
-//   contentOfFile,
-//   sha
-// });
+console.log(`File updated. New commit: ${updateResult.data.commit.sha}`);
 
 
+// 5 .Create a new Branch.
 
+// Define the branch name
+const branchName = "new-branch";
 
+// Get the SHA of the master branch
+const { data: masterBranch } = await octokit.request(`GET /repos/${payload.repository.owner.login}/${payload.repository.name}/git/ref/heads/master`);
+const masterSha = masterBranch.object.sha;
 
-// 5 .
-// const owner = "OWNER_USERNAME";
-// const repo = "REPO_NAME";
-// const baseBranch = "BASE_BRANCH_NAME";
-
-// const refResult = await octokit.git.getRef({
-//   owner,
-//   repo,
-//   ref: `heads/${baseBranch}`
-// });
-
-// const baseSha = refResult.data.object.sha;
-// const newBranch = "NEW_BRANCH_NAME";
-
-// await octokit.git.createRef({
-//   owner,
-//   repo,
-//   ref: `refs/heads/${newBranch}`,
-//   sha: baseSha
-// });
-
-
-  //   // Push data to external app
-  //   const data = {
-  //     pull_request_number: payload.pull_request.number,
-  //     issueTitle: payload.pull_request.title,
-  //   };
-
-  //   const response = await axios.post('www.google.com', data);
-
-  //   // Update Github repository with external app response
-  //   const externalAppResponse = response.data;
-  //   const message = `External app responded with: ${externalAppResponse}`;
-
-  //   await octokit.issues.createComment({
-  //     owner:payload.repository.owner.login,
-  //     repo: payload.repository.name,
-  //     issue_number: payload.number,
-  //     body: message,
-  //   });
+// Create a new reference with the SHA of the master branch
+await octokit.request(`POST /repos/${payload.repository.owner.login}/${payload.repository.name}/git/refs`, {
+  ref: `refs/heads/${branchName}`,
+  sha: masterSha
+})
+.then((response)=>{
+  console.log("Successfully created new branch",response.data)
+})
+.catch((error)=>{
+  console.log("Something went Wrong",error)
+})
+} catch (error) {
+  console.error('Error sending request to third-party API:', error.message);
+}
   }
 
   res.sendStatus(200);
@@ -153,5 +137,4 @@ console.log( comment.body)
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
-
 
